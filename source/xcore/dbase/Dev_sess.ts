352307d7e8093758d5aab6861635ec44fc80f1e5
tomboygink <49483215+tomboygink@users.noreply.github.com>
2023-02-17 10:03:08 +0500

    17.02.2023_10.03
    17.02.2023_10.03

--- source/xcore/dbase/Dev_sess.ts
+++ source/xcore/dbase/Dev_sess.ts
@@ -1,4 +1,5 @@
 import { DBase, getDB } from "./DBase";
+import { dateTimeToSQL } from '../../xcore/dbase/DateStr'
 
 export class Dev_sessEntity{
     id:number = 0;
@@ -9,4 +10,34 @@ export class Dev_sessEntity{
     level_akb:number = 0;
     sess_data:string = '';
     constructor(){}
+}
+
+export class Dev_sessTable
+{
+    db: DBase;
+    args: any;
+    sess_code: string;
+    constructor(_args: any, _sess_code: string) {
+        this.db = getDB();
+        this.args = _args;
+        this.sess_code = _sess_code;
+    }
+
+    //Получение сессий устройства за определенный период
+    async selectDevSess(){
+        var start_date = new Date(this.args.sess_period_start).toISOString().slice(0, 19).replace('T', ' ');
+        var end_date = new Date(this.args.sess_period_end).toISOString().slice(0, 19).replace('T', ' ');
+        //console.log(start_date);
+        //console.log(end_date);
+
+       // console.log("SELECT * FROM SelectDev_Sess ('"+this.args.dev_number+"', '"+start_date+"', '"+end_date+"')");
+
+        var db_res = await this.db.query("SELECT * FROM SelectDev_Sess ('"+this.args.dev_number+"', '"+start_date+"', '"+end_date+"')");
+        var result: Dev_sessEntity[] = new Array();
+        for (var r in db_res.rows) {
+            result.push(db_res.rows[r]);
+        }
+        console.log(result);
+        return result;
+    }
 }
