b28820d6965209ef133694db0a018dd8f9b54d5a
azatylnikova <zatylnikova@bvs45.ru>
2023-03-30 14:38:11 +0500

    30.03.2023 14:37

--- source/client/src/components/AppForm/Devs/Devs.tsx
+++ source/client/src/components/AppForm/Devs/Devs.tsx
@@ -43,6 +43,7 @@ export class Devs extends React.Component<IProps> {
 
         if ("_dev_id_key_" + gr_devs[key].id === dev.getIdChild()) {
           APP_STORAGE.sensors.setNumber(gr_devs[key].number);
+          APP_STORAGE.sensors.get_DevFirstLastSessions("sess_id", APP_STORAGE.auth_form.getdt(),  gr_devs[key].number);
           let longitude = gr_devs[key].longitude;
           let latitude = gr_devs[key].latitude;
 
