58cdae69da2e17e57c86bcb08e4af02883b2d1d1
azatylnikova <zatylnikova@bvs45.ru>
2023-07-04 14:34:09 +0500

    04.07.2023 14:34

--- source/client/src/components/AppForm/Sensors/SensorsList.tsx
+++ source/client/src/components/AppForm/Sensors/SensorsList.tsx
@@ -260,9 +260,19 @@ const [year_first, month_first, day_first] = FirstData.split('-');
              
               alignItems: "center",
             }}
+
           >
+            <svg className="svg_icon" width="32" height="32" viewBox="0 0 32 32" fill="none" xmlns="http://www.w3.org/2000/svg">
+<circle cx="16" cy="16" r="16" fill="#75A4FF"/>
+<path d="M24 10H8V12H6V10C6 8.89543 6.89543 8 8 8H24C25.1046 8 26 8.89543 26 10V22C26 23.1046 25.1046 24 24 24H19V22H24V10Z" fill="white"/>
+<path d="M6 17C9.86599 17 13 20.134 13 24H11C11 21.2386 8.76142 19 6 19V17Z" fill="white"/>
+<path d="M6 21C7.65685 21 9 22.3431 9 24H6V21Z" fill="white"/>
+<path d="M6 13C12.0751 13 17 17.9249 17 24H15C15 19.0294 10.9706 15 6 15V13Z" fill="white"/>
+</svg>
+
               <Typography sx={{fontSize: '14px', pl: '8px'}}>
-               С момента передачи данных прошло -  {APP_STORAGE.devs.getPassedDay()} дней
+              Прошло -  {APP_STORAGE.devs.getPassedDay()} дней 
+              <Typography sx={{fontSize: '12px'}}> с момента передачи данных</Typography>
               </Typography>
               <Box sx ={{background: '#75A4FF', p: '8px', borderRadius: '4px', height: '100%', display: 'flex', alignItems: 'center',
               borderTopLeftRadius: '0px',
@@ -290,6 +300,13 @@ const [year_first, month_first, day_first] = FirstData.split('-');
               alignItems: "center",
             }}
           >
+                   <svg className="svg_icon" width="32" height="32" viewBox="0 0 32 32" fill="none" xmlns="http://www.w3.org/2000/svg">
+<circle cx="16" cy="16" r="16" fill="#75A4FF"/>
+<path d="M24 10H8V12H6V10C6 8.89543 6.89543 8 8 8H24C25.1046 8 26 8.89543 26 10V22C26 23.1046 25.1046 24 24 24H19V22H24V10Z" fill="white"/>
+<path d="M6 17C9.86599 17 13 20.134 13 24H11C11 21.2386 8.76142 19 6 19V17Z" fill="white"/>
+<path d="M6 21C7.65685 21 9 22.3431 9 24H6V21Z" fill="white"/>
+<path d="M6 13C12.0751 13 17 17.9249 17 24H15C15 19.0294 10.9706 15 6 15V13Z" fill="white"/>
+</svg>
               <Typography sx={{fontSize: '14px', pl: '8px'}}>
                Нет передачи данных
               </Typography>
